04d467d55a9f4e5872ffd1f6d3d815a19febec93
Andrea Burns <aburns3@tulane.edu>
2017-10-17 21:19:24 +0000

    working C9 on Tuesday October 17

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20171011150726) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lines", force: :cascade do |t|
     t.string "train"
     t.string "color"
