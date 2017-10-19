dc473d003d9c70289c97401e237496e9ccd4bcd4
Andrea Burns <aburns3@tulane.edu>
2017-10-19 02:25:52 +0000

    preparing for merge again

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resource :stops do
     get "stop"
   end
+  resources :stops, path: "stops", as: "stops"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
