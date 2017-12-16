8b5624699d36e1dc823ee68ad3812dd7adda3988
Andrea Burns <aburns3@tulane.edu>
2017-12-16 17:46:44 +0000

    make maximum description length longer

--- app/models/line.rb
+++ app/models/line.rb
@@ -4,7 +4,7 @@ class Line < ActiveRecord::Base
     
     validates :train, presence: {message: "Please name the train."}
     validates :description, presence: {message: "Please write a description."}
-    validates :description, length: { in: 5..100}
+    validates :description, length: { in: 5..1000}
     
     has_attached_file :image
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
