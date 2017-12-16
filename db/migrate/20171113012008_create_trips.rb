1f0cd7cc1009caca5f3c7e35ceb9d75a57ac70e3
Andrea Burns <aburns3@tulane.edu>
2017-12-16 00:46:46 +0000

    commented out error-inducing line in CreateTrips migration, added images for train details

--- db/migrate/20171113012008_create_trips.rb
+++ db/migrate/20171113012008_create_trips.rb
@@ -2,7 +2,7 @@ class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
       t.string :train
-      t.current_passenger.name :passenger_name, array: true
+      #t.current_passenger.name :passenger_name, array: true
       t.timestamps
     end
   end
